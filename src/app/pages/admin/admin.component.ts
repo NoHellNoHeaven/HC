611115a6907f2f6a15f915fd49402bd16b795f2f
NoHellNoHeaven <espindolaalex627@gmail.com>
2025-06-18 20:51:01 -0400

    holamundo

---
+++ src/app/pages/admin/admin.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-admin',
+  template: `<h1>Admin Page</h1>`,
+  styles: [`h1 { color: blue; }`]
+})
+export class AdminComponent {}
---
+++ src/app/pages/admin/camiones/camiones.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-camiones',
+  imports: [],
+  templateUrl: './camiones.component.html',
+  styleUrl: './camiones.component.scss'
+})
+export class CamionesComponent {
+
+}
