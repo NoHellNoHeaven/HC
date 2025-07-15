f42ec9989300a5fea40102504c3f7cc2032290cb
NoHellNoHeaven <espindolaalex627@gmail.com>
2025-07-14 22:45:31 -0400

    requestDatos

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { authInterceptor } from './app/interceptor/auth.interceptor';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
-    ...appConfig.providers ?? [],
-    provideHttpClient()
+    ...(appConfig.providers ?? []),
+    provideHttpClient(withInterceptors([authInterceptor]))
   ]
 }).catch((err) => console.error(err));
